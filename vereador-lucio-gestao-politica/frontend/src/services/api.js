db6d361f91ee5840f506c882472709a22f447c41
EduuardoLS <carloseduardo13372022@gmail.com>
2025-12-05 01:09:56 -0300

    Fix: Configurando api.js para usar URL de Producao

--- vereador-lucio-gestao-politica/frontend/src/services/api.js
+++ vereador-lucio-gestao-politica/frontend/src/services/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
+// Lógica inteligente:
+// 1. Se tiver a variável da Vercel (Produção), usa ela.
+// 2. Se não tiver (Desenvolvimento), usa o localhost.
+// OBS: Adicionamos o '/api' no final automaticamente se a variável não tiver.
+
+const baseUrl = process.env.REACT_APP_API_URL 
+  ? `${process.env.REACT_APP_API_URL}/api` // Na Vercel: https://.../api
+  : 'http://localhost:5000/api';           // No seu PC
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: baseUrl
 });
 
 // Interceptador que adiciona o token a cada requisição
